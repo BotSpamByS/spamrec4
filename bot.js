509c4449b7666c3a499820d90c33737efb525ae3
BotSpamByS <48106207+BotSpamByS@users.noreply.github.com>
2019-03-16 09:55:44 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("555983666699042841")
+setInterval(function() {
+channel.send(`Spam Hmddddddd !!!`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
